a1a24712473b791d224fd5a9ed4d60d51c07b9e5
Jacob <jacobedwardhaig@gmail.com>
2020-08-10 11:34:32 -0400

    Added comments and cleaned up some code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::fs;
-use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
+    // Use clap as a CLI manager of sorts.
+    // Clap handles all flags to the program.
     let matches = clap::App::new("FileMoveProgram")
         .version("0.2")
         .about("Moves files from one folder to another")
@@ -35,12 +36,11 @@ fn main() {
     println!("Folder In {}\nFolder Out {}\n", folder_in, folder_out);
 
     loop {
-        let list_files = find_files(&folder_in);
-
-        for file in list_files {
+        for file in find_files(&folder_in) {
             let path = file.unwrap().path();
 
             if !path.is_dir() {
+                // Return just the name of the file.
                 let file_name = path.file_name().unwrap().to_str().unwrap();
 
                 println!("File {}", file_name);
@@ -51,38 +51,55 @@ fn main() {
     }
 }
 
+/// Gets a list of all items in a folder.
+/// Takes a Folder as an Argument.
+/// Throws Error if path to file is wrong.
+/// Returns an iterator of type ReadDir
 fn find_files(folder_in: &str) -> fs::ReadDir {
-    fs::read_dir(folder_in).expect("Error in find_files()")
+    fs::read_dir(folder_in).expect("Folder does not Exist. ")
 }
 
+/// Simple function to move the file from one folder to another
 fn move_file(file_name: &str, folder_in: &str, folder_out: &str) {
     let contents = read_file(folder_in, file_name);
     write_file(contents, folder_out, file_name);
-    delete_file(folder_in, file_name)
+    delete_file(folder_in, file_name);
 }
 
+/// Read a file in to memory as a Vector of Bytes.
+/// Takes a Folder and a File name as Arguments.
+/// Throws Error if path to file is wrong or if application
+/// doesnt have Permission to access the file.
 fn read_file(folder_in: &str, file_name: &str) -> Vec<u8> {
-    let file_result = File::open([folder_in, file_name].join(r"\"));
+    let file_result = fs::File::open([folder_in, file_name].join(r"\"));
 
-    let mut file = file_result.expect("File dont exist or something");
+    let mut file = file_result.expect("File does not exist or is inaccessable");
 
     let mut contents = Vec::new();
     file.read_to_end(&mut contents)
-        .expect("Cant retreive contents from file");
+        .expect("Cant retreive contents from file. ");
 
     contents
 }
 
+/// Creates and write contents to a file.
+/// Takes File name, Folder, and Vector of Bytes as Arguments.
+/// Throws Error if file can not be created.
 fn write_file(contents: Vec<u8>, folder_out: &str, file_name: &str) {
-    let file_result = File::create([folder_out, file_name].join(r"\"));
+    let file_result = fs::File::create([folder_out, file_name].join(r"\"));
 
-    let mut file = file_result.expect("Can not create the file sucker");
+    let mut file = file_result.expect("Can not create the file. ");
 
-    file.write_all(&contents).expect("Cant write or something");
+    file.write_all(&contents)
+        .expect("Can not write or something. ");
 }
 
+/// Removes a file from the filesystem.
+/// Takes a Folder and a File name as Arguments.
+/// Throws Error if path to file is wrong or if application
+/// doesnt have Permission to access the file.
 fn delete_file(folder_in: &str, file_name: &str) {
-    fs::remove_file([folder_in, file_name].join(r"\")).expect("Cant remove the files!");
+    fs::remove_file([folder_in, file_name].join(r"\")).expect("Can not remove the files. ");
 }
 
 #[test]
